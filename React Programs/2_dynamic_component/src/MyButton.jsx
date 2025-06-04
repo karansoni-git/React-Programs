625e75d0b8b6374c227db45bc65fb455cde6323b
karan soni <karanparekh1011@gmail.com>
2025-06-04 23:28:05 +0530

    Add files via upload

---
+++ React Programs/2_dynamic_component/src/MyButton.jsx
@@ -0,0 +1,6 @@
+function MYButton(){
+    //here we have add a bootstrap to style a button is not neccesary.
+    return <button class="btn btn-danger">click me</button>
+}
+
+export default MYButton;
