57d72196a1a5cb3f25f199fa8c1ea9b97c207df6
karan soni <karanparekh1011@gmail.com>
2025-06-06 21:57:40 +0530

    Add files via upload

---
+++ React Programs/4_map_method/src/App.js
@@ -0,0 +1,13 @@
+import Heading from "./components/Heading";
+import Data from "./components/Data";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <Heading />
+      <Data />
+    </>
+  );
+}
+
+export default App;
